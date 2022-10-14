ec21de7369a01fd6dd06be6773b591fadc8174ba
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-10-14 14:35:44 -0400

    worked on dashboard routes

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -2,4 +2,44 @@ const router = require('express').Router();
 const { User, Post, Comments } = require('../models');
 const withAuth = require('../utils/auth');
 
+//get all posts
+router.get('/',withAuth, async (req, res) => {
+    try {
+      // Get all posts and JOIN with user data
+      const postData = await Post.findAll({
+        attributes: [
+          'id',
+          'title',
+          'content',
+          'date_created'
+        ],
+        include: [
+          {
+            model: Comments,
+            attributes: ['id', 'comment', 'post_id', 'user_id', 'date_created'],
+            include: {
+              model: User,
+              attributes: ['username']
+            }
+          },
+          {
+            model: User,
+            attributes: ['username']
+          }
+        ],
+      });
+  
+      // Serialize data so the template can read it
+      const posts = postData.map((post) => post.get({ plain: true }));
+  
+      // Pass serialized data and session flag into template
+      res.render('dashboard', {
+        posts,
+        logged_in: req.session.logged_in
+      });
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -45,7 +45,7 @@ router.get('/', async (req, res) => {
 
 
 //get one post by id
-router.get('/:id', async (req, res) => {
+router.get('/post/:id', async (req, res) => {
   try {
     const postData = await Post.findByPk(req.params.id, {
       attributes: [
