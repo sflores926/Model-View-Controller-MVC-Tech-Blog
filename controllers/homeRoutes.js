5a1f354e9a4dead24421d01338bc67a9e3fef08b
Stephany Flores <106363593+sflores926@users.noreply.github.com>
2022-10-17 08:55:48 -0400

    homeroutes changes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -113,7 +113,7 @@ router.get('/dashboard', withAuth, async (req, res) => {
 router.get('/login', (req, res) => {
   // If the user is already logged in, redirect the request to another route
   if (req.session.logged_in) {
-    res.redirect('/');
+    res.redirect('/theirDashboard');
     return;
   }
 
